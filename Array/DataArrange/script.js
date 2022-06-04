df83e9af9bcecbe9b501cdcaa06a1a6bae8e222f
Siddharth <sidd4038@gmail.com>
2022-06-04 13:03:19 +0530

    commit

---
+++ Array/DataArrange/script.js
@@ -0,0 +1,22 @@
+var employes = [
+    {firstName: "test", lastName : "patel", city: "pune"},
+    {firstName: "samarth", lastName : "shah",city: "Ahemedabad"},
+    {firstName: "saurabh", lastName : "sorathyia", city:"Nodia"},
+    {firstName: "shreyash", lastName : "jain",city : "vadodara"},
+    {firstName: "test", lastName : "patel", city:"vadodara"},
+    {firstName: "mukesh", lastName : "jain",city:"test"}
+
+]
+//{"shreyash","test"}
+// var data = employes.filter(x => x.city == 'vadodara').map(x => x.firstName)
+
+// console.log(data);
+
+var data = employes.reduce(function(acc,curr){
+      if(curr.city == 'vadodara'){
+          acc.push(curr.firstName)
+      }
+      return acc;
+},[])
+
+console.log(data);
