94300a7ed162e078c9f192bf747e77372f453d8d
Siddharth <sidd4038@gmail.com>
2022-07-09 22:25:48 +0530

    commit

--- product_new/product.js
+++ product_new/product.js
@@ -2,77 +2,69 @@
 
 var dataArray = [];
 
+const display = () => {
 
-const displayMyHtml = (dataArray,isload)=> {
+  fetch('https://fakestoreapi.com/products')
+    .then(x => x.json())
+    .then(x => {
+      dataArray = x;
+      displayMyHtml(x, true);
+    })
+}
 
- const   data=  dataArray.map((value) => {
-        return `<article class="product" ondblclick="showDetail(event)" id="${value.id}">
+const displayMyHtml = (dataArray, isload) => {
+
+  const data = dataArray.map((value) => {
+    return `<article class="product" ondblclick="showDetail(event)" id="${value.id}">
 <img src="${value.image}" class="product-img img" alt="" />
 <footer>
     <h5 class="product-name">${value.title}</h5>
     <span class="product-price">$${value.price}</span>
 </footer>
 </article>`
-    })
-    document.getElementById("items").innerHTML = data.join(" ");
+  })
+  document.getElementById("items").innerHTML = data.join(" ");
 
-    if(isload)
-    {
+  if (isload) {
 
     const btn = dataArray.map((value) => {
-        return `<div class="row1"><input type="checkbox" class="inputcheck" value="${value.category}" id="${value.category}" onclick="filter_info(event)"><button class="company-btn">${value.category}</button></div>`
+      return `<div class="row1"><input type="checkbox" class="inputcheck" value="${value.category}" onclick="filter_info(event)"><button class="company-btn">${value.category}</button></div>`
     });
 
     const filt_btn = [...new Set(btn)];
     //console.log(filt_btn);
 
     document.getElementById("categories").innerHTML = filt_btn.join(" ");
-}
+  }
 
-}
-
-
-
-const display = () => {
-
-    fetch('https://fakestoreapi.com/products')
-        .then(x => x.json())
-        .then(x => {
-            dataArray = x;
-          displayMyHtml(x,true);
-        })
 }
 
 display();
 
-const showDetail = (e) => {
-    console.log(e.target);
-}
-
+// const showDetail = (e) => {
+//   console.log(e.target);
+// }
 
 function filter_info(e) {
-    var array = []
-    var  ty = document.querySelectorAll('.inputcheck:checked');
+  var array = []
+  var ty = document.querySelectorAll('.inputcheck:checked');
 
-    console.log(ty);
+  if (ty.length > 0) {
     for (var i = 0; i < ty.length; i++) {
-        array.push(ty[i].value)
-      }
+      array.push(ty[i].defaultValue)
+    }
 
-    //   if(array.length )
+    var myfilterData = dataArray.filter((value) => {
 
-      var myfilterData = dataArray.filter((value)=> {
+      return array.includes(value.category)
+    });
 
-        return array.includes(value.category)
-      });
+    displayMyHtml(myfilterData, false);
 
-
-
-
-     displayMyHtml(myfilterData);
-      
-
-   
+  }
+  else {
+    displayMyHtml(dataArray, false);
+  }
 
 }
 
